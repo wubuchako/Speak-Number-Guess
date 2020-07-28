fa07b37e3a870aca5b5a15c40c3091fc9b069784
wubuchako <56101474+wubuchako@users.noreply.github.com>
2020-07-28 21:31:54 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const msgEl = document.getElementById('msg');
+
+const randomnum = getRandomNumber();
+
+console.log(randomNum);
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+}
